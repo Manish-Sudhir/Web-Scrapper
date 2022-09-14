cce08aa28148914399befd20432c30e9d6bd9684
Manish-Sudhir <manishsudhir8@gmail.com>
2022-09-14 01:56:13 +0100

    updated docstrings and readme

---
+++ README.md
@@ -0,0 +1,10 @@
+This Project aims to create a data collection pipeline to scrape a teams information from a football analytics web-site (using selenium) and store the data retrived into a postgres database stored on a cloud via Amazon aws.
+This project utilises data engineering philosophies and technologies.
+Python packages used are:
+selenium
+webdriver_manager
+pandas
+sqlalchemy
+psycopg2-binary
+
+A RDS and an EC2 Instance are also utilised in AWS to store the data scrapped in the cloud.
--- utils/mal.py
+++ utils/mal.py
@@ -10,6 +10,9 @@ from selenium.webdriver.chrome.options import Options
 import time
 import pandas as pd
 
+"""
+Generic Scrapper class that hosts functions that are to be used by the scrapper
+"""
 class Scrapper:
     def __init__(self,url: str='https://www.transfermarkt.com/', headless: bool =True):
         if headless:
@@ -26,27 +29,64 @@ class Scrapper:
         self.driver.get(url) 
 
     def accept_cookies(self, xpath: str = "//button[contains(@title,'ACCEPT ALL')]"):
+        """
+        Finds and clicks the "accept cookies" button in the frame
+
+        parameters:
+        xpath : str
+                The Xpath of the accept cookies button in the frame
+        """
         WebDriverWait(self.driver,10).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,'//iframe[@id="sp_message_iframe_575843"]')))
         accept_cookies= WebDriverWait(self.driver,10).until(EC.element_to_be_clickable((By.XPATH,xpath)))
         accept_cookies.click()
     
     def click_element(self, xpath: str):
+        """
+        Clicks a element
+
+        parameters:
+        xpath : str
+                The Xpath of the element
+        """
 
         element = self.driver.find_element(By.XPATH, xpath)
         element.click()
 
     def find_elements_in_container(self, xpath_container: str, 
                                     tag_elements: str) -> list:
+        """
+        Clicks a element in a container 
+
+        parameters:
+        xpath_container : str
+                The Xpath of the container
+        tag_elements : str
+                The Xpath of the elements to be found in the container
+        """
 
         container = self.driver.find_element(By.XPATH, xpath_container)
         elements_in_container = container.find_elements(By.XPATH, f'./{(tag_elements)}')
 
         return elements_in_container
 
+"""
+Child class of the Scrapper class
+This class is made according to the use case of the web-scrapper
+This project scrapes a football analytics site
+"""
 class PlayerScrapper(Scrapper):
     def search_team(self,
                     team:str,
                     xpath_search_bar: str = '//*[@id="schnellsuche"]/input[1]'):
+        """
+        Function that searches for a particular team on the search bar
+
+        parameters:
+        team : str
+                name of the team to be searched
+        xpath_search_bar : str
+                xpath of the search bar on the webpage
+        """
         self.accept_cookies()
         time.sleep(1)
         print("Searching team..")
@@ -61,18 +101,25 @@ class PlayerScrapper(Scrapper):
         top_result.click()
 
     def get_info(self):
+        """
+        Function that scrapes the team information from the webpage 
+        """
+        # Storing data retrieved from webpage table into variables
         kit_numbers = self.driver.find_elements(By.XPATH, '//table[@class="items"]/tbody/tr/td[1]')
         player_names = self.driver.find_elements(By.XPATH, '//table[@class="items"]/tbody/tr/td[2]')
         player_dob = self.driver.find_elements(By.XPATH, '//table[@class="items"]/tbody/tr/td[4]')
         market_value = self.driver.find_elements(By.XPATH, '//table[@class="items"]/tbody/tr/td[6]')
         name=[]
         position=[]
+        # Player name column on webpage table holds player name and position
+        # So splitting that information here into different variables
         for p in player_names:
             hold = p.text
             hold.split('\n')
             name.append(hold.split('\n')[0])
             position.append(hold.split('\n')[1])
 
+        # Create pandas dataframe with scrapped informationo
         df_team_info = pd.DataFrame(columns=['Kit Number', 'Name', 'Position', 'Date Of Birth/Age', 'Market Value'])
         for i in range(len(kit_numbers)):
             df_team_info = df_team_info.append({'Kit Number': kit_numbers[i].text,
